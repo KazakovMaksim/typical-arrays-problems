167ba1d3ab66c8458f1bc32c51d08ec66913ea42
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-02-20 17:33:46 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  }
+  else {
+    let min = array[0];
+    let i = 0;
+    while (i++ < array.length) {
+        if (array[i] < min) {
+            min = array[i];
+        } 
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0){
+    return 0;
+  } else {
+    let max = array[0];
+    let i = 0;
+    while (i++ < array.length) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  } else {
+    let average = 0;
+    let i = 0;
+      do { 
+        average += array[i];
+         } 
+      while (++i < array.length)
+      
+    return average/array.length;
+  } 
 }
